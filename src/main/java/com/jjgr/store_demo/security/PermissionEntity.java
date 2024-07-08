2db48163746d7773468cc7891d5e71e00575e6c1
Jesús Gaytán Ramírez <jesusgaytanramirez144@gmail.com>
2024-07-08 01:12:14 -0600

    Implementación de PermissionEntity

---
+++ src/main/java/com/jjgr/store_demo/security/PermissionEntity.java
@@ -0,0 +1,23 @@
+package com.jjgr.store_demo.security;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class PermissionEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    
+    private String name;
+}
